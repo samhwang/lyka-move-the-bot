7b1164ea30448acfe6affaf631bfa4a6e5cf4a62
Sam Huynh <sam.huynh@lyka.com.au>
2024-05-02 23:17:13 +1000

    feat: update initial crate state

--- bin/move.ts
+++ bin/move.ts
@@ -1,23 +1,24 @@
 import { type Coordinates, type FactoryState, execute } from '../src';
 
 // @TODO: Edit this with the starting position of the Robot.
-const COMMAND = '';
+const COMMAND = 'N E N E N E N E G';
 const ROBOT_POSITION: Coordinates = [0, 0];
 const GRID_STATS: FactoryState = {
   robot: {
     position: ROBOT_POSITION,
     hasCrate: false,
   },
-  crates: [],
+  crates: [{ position: [4, 4] }, { position: [9, 9] }],
 };
 
 function moveTheRobot() {
   // Script Execution step. DO NOT EDIT.
   const finish = execute(GRID_STATS, COMMAND);
 
-  console.log('STARTING POSITION: ', ROBOT_POSITION);
+  console.log('STARTING POSITION: ', JSON.stringify(GRID_STATS));
+  console.log('COMMAND: ', COMMAND);
   console.log('FINISHED MOVING.');
-  console.log('GRID STATE: ', finish);
+  console.log('GRID STATE: ', JSON.stringify(finish));
 }
 
 moveTheRobot();
